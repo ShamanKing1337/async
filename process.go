71325f09851c47538b8d7fb4aa14a5478483a8e7
ShamanKing1337 <50265640+ShamanKing1337@users.noreply.github.com>
2021-02-28 23:09:03 +0300

    Add files via upload

--- process.go
+++ process.go
@@ -1,10 +1,9 @@
 package main
 
-
 import (
-"database/sql"
-"fmt"
-_ "github.com/lib/pq"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
 	"time"
 )
 
@@ -17,8 +16,7 @@ type Request struct{
 }
 
 
-func main(){
-
+func report(){
 	for true{
 		connStr := "user=postgres password=zopa123 dbname=postgres sslmode=disable host=localhost"
 		db, err := sql.Open("postgres", connStr)
@@ -26,29 +24,36 @@ func main(){
 			panic(err)
 		}
 		defer db.Close()
-		rows,err := db.Query("SELECT id, data, version FROM  reqests where isproceeded is NULL LIMIT 1 FOR UPDATE")
-		requests := []Request{}
+		tr, _ := db.Begin()
+		rows := tr.QueryRow("SELECT id, data, version FROM  reqests where isproceeded is NULL LIMIT 1 for update SKIP LOCKED")
+		if err != nil{
+			fmt.Println(err)
+		}
+		p := Request{}
+		rows.Scan(&p.Id, &p.Amount, &p.Version)
+		time.Sleep(10 * time.Second)
 
-		for rows.Next(){
-			p := Request{}
-			err := rows.Scan(&p.Id, &p.Amount, &p.Version)
-			if err != nil{
-				fmt.Println(err)
-				continue
-			}
-			requests = append(requests, p)
-			db.Exec("UPDATE reqests SET isproceeded = 'True' where id = $1", p.Id)
+
+		_, err =tr.Exec("UPDATE reqests SET isproceeded = 'True' where id = $1", p.Id)
+		if err != nil{
+
+			fmt.Println(err)
+		}
 			fmt.Println(p.Id)
 
-		}
 
-		fmt.Println(requests)
+		tr.Commit()
 
-		time.Sleep(10 * time.Second)
+
+
 	}
 
+}
 
+func main(){
 
+	report()
 
+	//time.Sleep(100 * time.Second)
 }
 
